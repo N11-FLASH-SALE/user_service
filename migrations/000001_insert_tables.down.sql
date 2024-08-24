1d9a70a2b4e4f90d4305189c3b0e2f6897e944ea
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-08-24 11:53:20 +0500

    all

--- migrations/000001_insert_tables.down.sql
+++ migrations/000001_insert_tables.down.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS users;
 DROP TYPE IF EXISTS roles;
+DROP TABLE IF EXISTS notifications;
--- migrations/000001_insert_tables.up.sql
+++ migrations/000001_insert_tables.up.sql
@@ -7,8 +7,25 @@ CREATE TABLE IF NOT EXISTS users (
     full_name VARCHAR(100),
     phone_number VARCHAR(20),
     address VARCHAR(255),
+    photo VARCHAR(100),
     role roles NOT NULL DEFAULT 'consumer',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     deleted_at Bigint DEFAULT 0
+);
+
+
+CREATE TABLE IF NOT EXISTS cards (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    card_number VARCHAR(16) NOT NULL,
+    expiration_date DATE NOT NULL,
+    security_code VARCHAR(4) NOT NULL,
+)
+
+CREATE TABLE IF NOT EXISTS notifications(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    messages TEXT NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE
 );
